85855e7653f6667539ce8cde300d28af1a43d96e
Oxymium <ffepoch@free.fr>
2021-06-14 14:35:27 +0200

    - Imported raw defence classes & their layouts

---
+++ app/src/main/java/com/oxymium/realestatemanager/misc/SecondActivity.java
@@ -0,0 +1,17 @@
+package com.oxymium.realestatemanager.misc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.oxymium.realestatemanager.R;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+}
